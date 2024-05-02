153a7f8b5a98eb5cbeb878202254cdd14e5222b7
oHminod <yvesm13@gmail.com>
2024-05-02 18:46:41 +0200

    images + style

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
     content: ["**/*.{html,js}"],
     theme: {
         extend: {
+            colors: {
+                primary: "#FFD15B",
+                iconBlack: "#1B1B1B",
+                greybg: "#EDEDED",
+                greytext: "#7A7A7A",
+            },
             fontFamily: {
                 sans: ["Manrope", "sans-serif"],
                 anton: ["Anton", "sans-serif"],
