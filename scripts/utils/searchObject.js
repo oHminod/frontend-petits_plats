6d4bd2ff781cc6caa75a398ac6029b54f2bcfef7
oHminod <yvesm13@gmail.com>
2024-05-18 17:16:22 +0200

    Ajout readMe et jsdoc dans searchobject

--- scripts/utils/searchObject.js
+++ scripts/utils/searchObject.js
@@ -12,6 +12,10 @@ export const searchObject = {
     ustensilsTags: new Set(),
     searchField: "",
 
+    /**
+     * Définit la valeur du champ de recherche et met à jour les recettes filtrées en conséquence.
+     * @param {string} tag - La nouvelle valeur du champ de recherche.
+     */
     setSearchField(tag) {
         const previousLength = this.searchField.length;
         this.searchField = tag;
@@ -24,6 +28,11 @@ export const searchObject = {
         }
     },
 
+    /**
+     * Ajoute un tag à l'ensemble de tags spécifié et met à jour les recettes filtrées.
+     * @param {string} tagType - Le type de tag à ajouter (par exemple, "ingredientTags").
+     * @param {string} tag - Le tag à ajouter.
+     */
     _addTag(tagType, tag) {
         tag = tag.toLowerCase();
         if (!this[tagType].has(tag)) {
@@ -44,6 +53,12 @@ export const searchObject = {
         this._addTag("ustensilsTags", tag);
     },
 
+    /**
+     * Supprime un tag de l'ensemble de tags spécifié et met à jour les recettes filtrées.
+     * @param {string} tagType - Le type de tag à supprimer (par exemple, "ingredientTags").
+     * @param {string} tag - Le tag à supprimer.
+     * @param {boolean} removeTagFromUI - Indique si le tag doit être supprimé de l'interface utilisateur.
+     */
     _removeTag(tagType, tag, removeTagFromUI = false) {
         tag = tag.toLowerCase();
         this[tagType].delete(tag);
@@ -77,6 +92,12 @@ export const searchObject = {
         }
     },
 
+    /**
+     * Définit la liste de tags pour le type de tag spécifié à partir des recettes fournies.
+     * @param {string} tagType - Le type de tag pour lequel définir la liste (par exemple, "ingredientTagsList").
+     * @param {string} property - La propriété de la recette à utiliser pour créer les tags.
+     * @param {Array} recipes - Les recettes à utiliser pour créer les tags.
+     */
     _setTagsList(tagType, property, recipes) {
         this[tagType] = recipes.reduce((acc, recipe) => {
             const items = Array.isArray(recipe[property])
@@ -100,6 +121,9 @@ export const searchObject = {
         this._setTagsList("ustensilsTagsList", "ustensils", filteredRecipes);
     },
 
+    /**
+     * Réinitialise l'objet de recherche à son état initial.
+     */
     reset() {
         this.filteredRecipes = [];
         this.ingredientTags = new Set();
@@ -117,6 +141,12 @@ export const searchObject = {
         );
     },
 
+    /**
+     * Vérifie si toutes les recettes contiennent le tag spécifié.
+     * @param {string} tag - Le tag à vérifier.
+     * @param {string} property - La propriété de la recette à vérifier.
+     * @returns {boolean} - Retourne true si toutes les recettes contiennent le tag, false sinon.
+     */
     allRecipesContainTag(tag, property) {
         tag = tag.toLowerCase();
         if (property === "removeIngredientTag") property = "ingredients";
@@ -136,10 +166,24 @@ export const searchObject = {
         }
         return false;
     },
+    /**
+     * Vérifie si tous les tags sont présents dans les éléments fournis.
+     * @param {Set} tags - Les tags à vérifier.
+     * @param {Set} items - Les éléments dans lesquels vérifier les tags.
+     * @returns {boolean} - Retourne true si tous les tags sont présents, false sinon.
+     */
     _checkTags(tags, items) {
         return [...tags].every((tag) => items.has(tag));
     },
 
+    /**
+     * Vérifie si le champ de recherche est présent dans le nom, la description ou les ingrédients de la recette.
+     * @param {string} searchField - Le champ de recherche à vérifier.
+     * @param {string} name - Le nom de la recette.
+     * @param {string} description - La description de la recette.
+     * @param {Array} ingredients - Les ingrédients de la recette.
+     * @returns {boolean} - Retourne true si le champ de recherche est présent, false sinon.
+     */
     _checkSearchField(searchField, name, description, ingredients) {
         return (
             searchField.trim() === "" ||
@@ -149,10 +193,23 @@ export const searchObject = {
         );
     },
 
+    /**
+     * Définit les recettes filtrées.
+     * @param {Array} recipes - Les recettes à définir comme filtrées.
+     */
     _setFilteredRecipes(recipes) {
         this.filteredRecipes = recipes;
     },
 
+    /**
+     * Filtre une recette en fonction des tags et du champ de recherche.
+     * @param {Object} recipe - La recette à filtrer.
+     * @param {Set} ingredientTags - Les tags d'ingrédients à vérifier.
+     * @param {Set} applianceTags - Les tags d'appareils à vérifier.
+     * @param {Set} ustensilsTags - Les tags d'ustensiles à vérifier.
+     * @param {string} searchField - Le champ de recherche à vérifier.
+     * @returns {boolean} - Retourne true si la recette passe le filtre, false sinon.
+     */
     _filterRecipe(
         recipe,
         ingredientTags,
@@ -183,6 +240,11 @@ export const searchObject = {
         return isTagFiltered && isSearchFiltered;
     },
 
+    /**
+     * Obtient les recettes filtrées en fonction des tags et du champ de recherche.
+     * @param {Array} recipesToFilter - Les recettes à filtrer (par défaut, toutes les recettes).
+     * @returns {Array} - Les recettes filtrées.
+     */
     getFilteredRecipes(recipesToFilter = recipes) {
         const start = performance.now();
         console.log("recipesToFilter", recipesToFilter.length);
